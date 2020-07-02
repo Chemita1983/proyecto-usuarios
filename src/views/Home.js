876f5d4e98dcbf0c11411f08e4d09723edd9bc12
chema <josemjimenezvega@gmail.com>
2020-07-02 19:17:06 +0200

    connect to API Rest and create login form

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,9 +1,43 @@
-import React, { Component } from "react";
+import React from "react";
+import Reflux from "reflux";
+import "../styles/Home.css";
 
-class Home extends Component {
+import AppStore from "../stores/AppStore";
+import Actions from "../actions/Actions";
+
+import Card from "../components/Card.js";
+
+class Home extends Reflux.Component {
+    constructor() {
+        super()
+
+        this.state = {};
+        this.store = AppStore;
+        this.storeKeys = ['data'];
+        this.showUsers = this.showUsers.bind(this);
+    }
+
+    showUsers(e) {
+        Actions.getUsers();
+    }
     render() {
+        let people = this.state.data.map((currentValue, index, array) => {
+            return (
+                <Card key={index} id={currentValue.id} first_name={currentValue.first_name} last_name={currentValue.last_name} avatar={currentValue.avatar}></Card>
+            )
+        });
+
         return (
-            <div>Home</div>
+            <div className="Home">
+                <div className="Home-header-box">
+                    <h2 className="Home-header-tittle">Welcome</h2>
+                    <button onClick={this.showUsers} className="Home-header-button">View Users</button>
+                </div>
+                <div className="Home-body">
+                    {people}
+                </div>
+            </div >
+
         );
     }
 }
--- src/views/Login.js
+++ src/views/Login.js
@@ -1,13 +1,60 @@
 import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import "../styles/Login.css";
 
 class Login extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            message: '',
+            username: '',
+            password: '',
+            isLogged: false
+        }
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onSubmit(e) {
+
+        e.preventDefault();
+        if (this.state.username === "admin" && this.state.password === "admin123") {
+            this.setState({ username: "", password: "", isLogged: true });
+        } else {
+            this.setState({ username: "", password: "", message: "Invalid credentials", isLogged: false });
+        }
+    }
+
     render() {
         return (
-            <div>
-                <label>Login</label>
-            </div>
+            <div className="Login">
+                {this.state.isLogged ?
+                    <Redirect to="/home/" />
+                    :
+                    <div className="Login-box">
+                        <div className="Login-message">
+                            <label>{this.state.message}</label>
+                        </div>
+                        <form onSubmit={this.onSubmit}>
+                            <div className="Login-box-formbox">
+                                <label className="Login-box-username">Username</label>
+                                <input type="text" value={this.state.username} onChange={(e) => { this.setState({ username: e.target.value }) }} className="Login-username-input" />
+                            </div>
+                            <div className="Login-box-formbox">
+                                <label className="Login-box-password">Password</label>
+                                <input type="password" value={this.state.password} onChange={(e) => { this.setState({ password: e.target.value }) }} className="Login-username-input" />
+                            </div>
+                            <button type="submit" className="Login-button-submit">Log in</button>
+                        </form>
+                    </div>
+                }
+            </div >
         );
     }
+
 }
+
+
+
 
 export default Login;
